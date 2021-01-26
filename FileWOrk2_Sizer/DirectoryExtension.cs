6c5ae14ad40f95b481c57d22797f932b5ea619d4
Роман Толмачёв <unifiq@gmail.com>
2021-01-26 23:48:29 +0700

    Добавьте файлы проекта.

---
+++ FileWOrk2_Sizer/DirectoryExtension.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+namespace FileWOrk2_Sizer
+{
+	public static class DirectoryExtension
+	{
+		public static string exceptedFolders = "";
+		static int deep = 0; //счётчик погружения
+		/// <summary>
+		/// рекурсивный подсчёт размера файлов в текущеё папке и всех её подкаталогах
+		/// </summary>
+		/// <param name="d"></param>
+		/// <returns></returns>
+		public static long DirSize(DirectoryInfo d)
+		{
+			long size = 0;
+			try
+			{
+				FileInfo[] files = d.GetFiles();
+				foreach (FileInfo f in files)
+				{
+					size += f.Length;
+				}
+
+				DirectoryInfo[] dirs = d.GetDirectories();
+				foreach (DirectoryInfo dir in dirs)
+				{
+					size += DirSize(dir);
+				}
+			}
+			catch (Exception e)
+			{
+				exceptedFolders += d.FullName + "\n";
+			}
+			return size;
+		}
+
+		/// <summary>
+		/// вывод содержимого папок с указанием размера файлов
+		/// </summary>
+		/// <param name="d"></param>
+		public static void SizeExplorer(DirectoryInfo d)
+		{
+			deep++; // визуализация погружения при выводе имени файла/папки
+			string result = "";
+			string tab = new String(' ', deep * 2); // отступ
+			// обход каталогов
+			try
+			{
+				DirectoryInfo[] dirs = d.GetDirectories();
+				foreach (DirectoryInfo dir in dirs)
+				{
+					result = String.Format("{0,-60}"
+						, String.Format("{0}[{1}]", tab, dir.Name)
+					);
+					Console.WriteLine(result);
+					SizeExplorer(dir);
+				}
+
+				// переходим к файлам
+				FileInfo[] fs = d.GetFiles();
+				foreach (FileInfo f in fs)
+				{
+					result = String.Format("{0,-40} {1,20:### ### ### ### ##0}"
+						, tab + f.Name
+						, f.Length
+					);
+					Console.WriteLine(result);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Папка не сдаётся по причине {0}", e.Message);
+			}
+			deep--;
+		}
+	}
+}
---
+++ FileWOrk2_Sizer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace FileWOrk2_Sizer
+{
+	//Напишите программу, которая считает размер папки на диске (вместе со всеми вложенными папками и файлами). 
+	//На вход метод принимает URL директории, в ответ — размер в байтах.
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Укажите путь к рабочей папке: ");
+			string workPath = Console.ReadLine();
+
+			if (Directory.Exists(workPath))
+			{
+				DirectoryInfo dir = new DirectoryInfo(workPath);
+				DirectoryExtension.SizeExplorer(dir);
+
+				Console.WriteLine("Общий размер папки: {0,20:### ### ### ### ##0}", DirectoryExtension.DirSize(dir));
+				if (DirectoryExtension.exceptedFolders.Length > 0)
+					Console.WriteLine("за исключением\n{0}", DirectoryExtension.exceptedFolders);
+			}
+			else
+			{
+				Console.WriteLine("Папка {0} не обнаружена", workPath);
+			}
+		}
+	}
+}
